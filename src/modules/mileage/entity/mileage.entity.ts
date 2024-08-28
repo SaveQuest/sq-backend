e6899474e1c00da23787ca2c9b7f50dc5be60eb6
PDH0129 <162543098+PDH0129@users.noreply.github.com>
2024-08-28 23:14:55 +0900

    사용량 저장로직 api에 맞게 수정

--- src/modules/mileage/entity/mileage.entity.ts
+++ src/modules/mileage/entity/mileage.entity.ts
@@ -3,9 +3,9 @@ import { User } from "@/modules/user/entities/user.entity";
 import { Column, CreateDateColumn, Entity, OneToOne, UpdateDateColumn } from "typeorm";
 
 @Entity()
-export class mileage {
+export class Mileage {
     @Exclude()
-    @Column()  
+    @Column()
     amount: number;
 
     @Column({ default: "" })
@@ -15,6 +15,34 @@ export class mileage {
     @CreateDateColumn({ type: "timestamptz" })
     spend_at: Date;
 
-    @OneToOne(() => User, user => user.userId, { onDelete: 'CASCADE' })  
+    @OneToOne(() => User, user => user.userId, { onDelete: 'CASCADE' })
     userId: User;
+
+    // cardIssuer: 카드사 (hanacard|kbcard|worricard|bccard|lottecard|kakaomini|tossuss)
+    @Column({ type: "varchar" })
+    cardIssuer: 'hanacard' | 'kbcard' | 'worricard' | 'bccard' | 'lottecard' | 'kakaomini' | 'tossuss';
+
+    // approvalNumber: 카드 승인 번호 (Optional)
+    @Column({ type: "varchar", nullable: true })
+    approvalNumber?: string;
+
+    // approvalTime: 카드 승인 일자 (Unix Timestamp)
+    @Column({ type: "int" })
+    approvalTime: number;
+
+    // merchant.name: 가맹점 이름
+    @Column({ type: "varchar" })
+    merchantName: string;
+
+    // merchant.category: 가맹점 업종 (Optional)
+    @Column({ type: "varchar", nullable: true })
+    merchantCategory?: string;
+
+    // merchant.id: 가맹점 번호 (Optional)
+    @Column({ type: "varchar", nullable: true })
+    merchantId?: string;
+
+    // merchant.businessNumber: 가맹점 사업자 번호 (Optional)
+    @Column({ type: "varchar", nullable: true })
+    merchantBusinessNumber?: string;
 }
