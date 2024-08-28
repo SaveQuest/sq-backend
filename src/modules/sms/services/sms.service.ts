dc126d17ef20198b264a428622ff6c08ddcc27d4
PDH0129 <162543098+PDH0129@users.noreply.github.com>
2024-08-28 23:49:40 +0900

    sms 로그인 성공메시지 웹훅으로 변경

--- src/modules/sms/services/sms.service.ts
+++ src/modules/sms/services/sms.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class SmsService {
+    private readonly webhookUrl = 'https://discord.com/api/webhooks/1278364638098231347/f0ir83VfJV6IW_TSRGZTGHPQh0ZeAMZoeygYhfY6U6qwkZyd_Ncmk46BkcifVVP8qm_A'; // 디스코드 웹훅 URL을 여기에 입력
+
     async sendSMS(phoneNumber: string, message: string) {
-        console.log(message);
+        const discordMessage = {
+            content: `Phone Number: ${phoneNumber}\nMessage: ${message}`,
+        };
+
+        try {
+            await axios.post(this.webhookUrl, discordMessage);
+        } catch (error) {
+            console.error('Error sending message to Discord ', error);
+        }
     }
 }
