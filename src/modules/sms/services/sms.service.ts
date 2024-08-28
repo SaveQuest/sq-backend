fdf6888dd4a8bcd30de6a19a8d00d41900ce6d16
PDH0129 <162543098+PDH0129@users.noreply.github.com>
2024-08-28 23:56:40 +0900

    웹훅 변경

--- src/modules/sms/services/sms.service.ts
+++ src/modules/sms/services/sms.service.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 @Injectable()
 export class SmsService {
-    private readonly webhookUrl = 'https://discord.com/api/webhooks/1278364638098231347/f0ir83VfJV6IW_TSRGZTGHPQh0ZeAMZoeygYhfY6U6qwkZyd_Ncmk46BkcifVVP8qm_A'; // 디스코드 웹훅 URL을 여기에 입력
+    private readonly webhookUrl = 'https://discord.com/api/webhooks/1278366685992325284/K2YADRPqsdAv0TU-NEEcmALBVfiIr8zG_vOQGtZxLvYsERlT3CH53_ZYSEzNFfponD9P'; // 디스코드 웹훅 URL을 여기에 입력
 
     async sendSMS(phoneNumber: string, message: string) {
         const discordMessage = {
