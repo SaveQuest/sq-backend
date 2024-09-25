72baa4dcff787aa8a44728101a3464c916b3e0f1
Yuchan Han <serltretu24@gmail.com>
2024-09-26 03:13:19 +0900

    docs 서버 추가

--- src/config/document.ts
+++ src/config/document.ts
@@ -6,6 +6,8 @@ export const DocumentConfig = new DocumentBuilder()
   .setDescription('API documentation for the SaveQuest mobile application.')
   .setVersion(version)
   .addServer('http://localhost:3000', 'Local development server')
+  .addServer('https://sq-production-v0.mintpasta.store/', 'Test Server (Production Mode)')
+  .addServer('https://savequest.ychan.me/', 'Production Server')
   .addBearerAuth(
     {
       type: 'http',
